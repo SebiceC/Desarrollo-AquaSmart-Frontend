12350b5542fa86cc2b8835d8474628083ea61ec7
Cristian Alvira <cristianvira123@gmail.com>
2025-03-10 16:13:11 -0500

    Protección de rutas cuando el usuario esta autenticado

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom' // Asegúrate de importar Navigate
-import Login from '../app/auth/Login'
-import PreRegister from '../app/auth/PreRegister'
-import Home from '../Home'
-import ForgotPassword from '../app/forgotPassword/ForgotPassword'
-import RecoverPassword from '../app/forgotPassword/RecoverPassword'
-import Perfil from '../app/Perfil'
-import IoTControll from '../app/IoTControll'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "../app/auth/Login";
+import PreRegister from "../app/auth/PreRegister";
+import Home from "../Home";
+import ForgotPassword from "../app/forgotPassword/ForgotPassword";
+import RecoverPassword from "../app/forgotPassword/RecoverPassword";
+import Perfil from "../app/Perfil";
+import IoTControll from "../app/IoTControll";
+import ProtectedRoute from "./ProtectedRoute"; // Importamos el componente de protección
 
 const AppRouter = () => {
-  return (
-    <Routes>
-        <Route path='/' element={<Navigate to='/login' replace />} />
-        <Route path='/home' element={<Navigate to="/perfil" replace />} />
-        <Route path='/preRegister' element={<PreRegister />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/forgotPassword' element={<ForgotPassword />} />
-        <Route path='/recoverPassword' element={<RecoverPassword />} />
-        <Route path='/perfil' element={<Perfil />} />
-        <Route path='/control-IoT' element={<IoTControll />} />
+    return (
+        <Routes>
+            {/* Redirección a login si no está autenticado */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/home" element={<Navigate to="/perfil" replace />} />
 
-        
-    </Routes>
-  )
-}
+            {/* Rutas públicas */}
+            <Route path="/preRegister" element={<PreRegister />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+            <Route path="/recoverPassword" element={<RecoverPassword />} />
+
+            {/* Rutas protegidas */}
+            <Route path="/perfil" element={<ProtectedRoute element={<Perfil />} />} />
+            <Route path="/control-IoT" element={<ProtectedRoute element={<IoTControll />} />} />
+        </Routes>
+    );
+};
 
 export default AppRouter;
