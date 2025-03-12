6ae9c20658d5506ee053d32b72817fed971fbc35
Andros178 <serdaniel01@gmail.com>
2025-03-12 15:57:00 -0500

    commit de prueba, no asustarse

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.test" });
-
-
-global.importMetaEnv = {
-    VITE_APP_API_URL: "http://localhost:5000", // Define tu API de prueba
-  };
-  
