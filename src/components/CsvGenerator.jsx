a2a19bc0c31e6f474adfed6e66470fe9071043eb
Jhon <jhonpulido762@gmail.com>
2025-03-30 09:18:49 -0500

    FIX: Corregir color de botones para descarga PDF y CSV

--- src/components/CsvGenerator.jsx
+++ src/components/CsvGenerator.jsx
@@ -64,10 +64,10 @@ const CSVDownloadButton = ({ data, startDate, endDate }) => {
   return (
     <button
       onClick={generateCSV}
-      className="flex items-center gap-2 bg-green-200 text-green-700 px-4 py-2 rounded-full text-sm hover:bg-green-300"
+      className="flex items-center gap-2 bg-[#4c84de] text-white px-4 py-2 rounded-full text-md font-semibold hover:bg-[#689ce6]"
     >
       <img src="/img/csv.png" alt="CSV Icon" width="20" height="20" />
-      <span>Descargar historial</span>
+      <span>Descargar CSV</span>
     </button>
   );
 };
