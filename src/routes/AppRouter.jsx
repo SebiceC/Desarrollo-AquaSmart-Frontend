0fdba1bf2e7bdf9321a5509492f3e368fdde1ad1
Esteban Escobar <juanes_escobar@hotmail.com>
2025-04-02 14:17:29 -0500

    Vistas para el control de valvulas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -44,6 +44,10 @@ import HistorialUserPredio from "../app/HistorialConsumo/HistorialUserPredioList
 import HistorialUserPredioDetail from "../app/HistorialConsumo/HistorialUserPredioDetail";
 import HistorialUserLoteDetail from "../app/HistorialConsumo/HistorialUserLoteDetail";
 
+import ValvesList from "../app/valvulas/ValvesList"
+import ValveDetail from "../app/valvulas/ValveDetail"
+import ValveFlowUpdate from "../app/valvulas/ValveFlowUpdate"
+
 
 const AppRouter = () => {
     return (
@@ -97,6 +101,9 @@ const AppRouter = () => {
             <Route path="/mispredios/historial-consumoList/:document" element={<ProtectedRoute element={<HistorialUserPredio />} />} />
             <Route path="/mispredios/historial-consumoPredio/:id_plot" element={<ProtectedRoute element={<HistorialUserPredioDetail />} />} />
             <Route path="/mispredios/historial-consumoPredio/:id_plot/milote/:id_lot" element={<ProtectedRoute element={<HistorialUserLoteDetail />} />} />
+            <Route path="/control-IoT/valvulas" element={<ProtectedRoute element={<ValvesList />} />} />
+            <Route path="/control-IoT/valvulas/:id_valve" element={<ProtectedRoute element={<ValveDetail />} />} />
+            <Route path="/control-IoT/valvulas/:id_valve/update-flow" element={<ProtectedRoute element={<ValveFlowUpdate />} />} />
         </Routes>
     );
 };
