43147bc00b8b003dcde85a485e4249cb8662f515
Esteban Escobar <juanes_escobar@hotmail.com>
2025-05-03 11:31:59 -0500

    El usuario puede solicitar la activacion del caudal

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -60,6 +60,7 @@ import ReportesYNovedades from "../app/reportes_y_novedades/solicitud_caudal/Rep
 import ReportesYNovedadesLotesList from "../app/reportes_y_novedades/solicitud_caudal/ReportesYNovedadesLotesList";
 import ReportarFallosList from "../app/reportes_y_novedades/reportar_fallos/ReportarFallosList";
 import CancelacionCaudal from "../app/reportes_y_novedades/solicitud_caudal/CancelacionCaudal";
+import ActivacionCaudal from "../app/reportes_y_novedades/solicitud_caudal/activar-caudal";
 
 const AppRouter = () => {
     return (
@@ -126,6 +127,7 @@ const AppRouter = () => {
             <Route path="/mis-facturas/detalle/:id_bill" element={<ProtectedRoute element={<MisFacturasDetails />} />} />
             <Route path="/reportes-y-novedades/solicitud_caudal" element={<ProtectedRoute element={<ReportesYNovedades />} />} />
             <Route path="/reportes-y-novedades/cancelacion_caudal" element={<ProtectedRoute element={<CancelacionCaudal />} />} />
+            <Route path="/reportes-y-novedades/activar-caudal" element={<ProtectedRoute element={<ActivacionCaudal />} />} />
             <Route path="/reportes-y-novedades/reportar_fallos" element={<ProtectedRoute element={<ReportarFallosList />} />} />
             <Route path="/reportes-y-novedades/lotes" element={<ProtectedRoute element={<ReportesYNovedadesLotesList />} />} />
         </Routes>
