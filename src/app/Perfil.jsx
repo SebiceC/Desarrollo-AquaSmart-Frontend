6e1eb07491ffd914154e4a777ca7e44f7c2443b4
Jhon <jhonpulido762@gmail.com>
2025-05-22 15:22:11 -0500

    FEAUTRE: Nuevo diseño de la vista de perfil2

--- src/app/Perfil.jsx
+++ src/app/Perfil.jsx
@@ -166,60 +166,6 @@ const Perfil = () => {
                         </div>
                     </div>
                 </div>
-                {/*
-                <div className="bg-gray-200 rounded-2xl p-8 shadow-md w-80 text-center relative">
-                    <div className="absolute top-4 right-4 text-gray-600 cursor-pointer flex flex-col items-center">
-                        <LogOut onClick={handleLogout} size={28} />
-                        <span className="text-xs">Cerrar sesión</span>
-                    </div>
-
-                    <div className="flex justify-center">
-                        <div className="w-24 h-24 rounded-full border-2 border-gray-400 flex items-center justify-center">
-                            <FaUser size={50} className="text-gray-500" />
-                        </div>
-                    </div>
-
-                    {user ? (
-                        <>
-                            <h2 className="mt-4 text-xl font-bold text-gray-900">{user.first_name + " " + user.last_name}</h2>
-                            <p className="text-gray-700 font-semibold">ID: {user.document}</p>
-
-                            <div className="mt-4 text-gray-700 space-y-2">
-                                <p className="flex items-center justify-center space-x-2">
-                                    <FaUser className="text-gray-600" />
-                                    <span>Persona: {user.person_type_name}</span>
-                                </p>
-                                <p className="flex items-center justify-center space-x-2">
-                                    <FaPhone className="text-gray-600" />
-                                    <span>{user.phone}</span>
-                                </p>
-                                <p className="flex items-center justify-center space-x-2">
-                                    <FaEnvelope className="text-gray-600" />
-                                    <span>{user.email}</span>
-                                </p>
-                            </div>
-                        </>
-                    ) : (
-                        <p className="text-gray-600 mt-4">Cargando perfil...</p>
-                    )}
-                    <div className="flex flex-col items-center gap-5">
-                        <button className="w-[50%] mt-4 bg-[#365486] text-white py-2  hover:bg-[#344663]"
-                            onClick={() => navigate('/perfil/actualizar-informacion')}>
-                            Editar
-                        </button>
-
-                        <Minus className="w-full h-[2px] bg-gray-400" />
-
-                        <button className="w-[70%] bg-[#365486] text-white py-2 px-2 hover:bg-[#344663]    "
-                            onClick={() => navigate('/mispredios/:document')}>
-                            Ver mis predios y lotes
-                        </button>
-
-                    </div>
-
-                    {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
-                </div>
-                */}
             </div>
         </div>
     );
